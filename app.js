1da341091bd082a22a7ca3e411d279dfa04d9ba8
José Antonio Reyes <imjaroiswebdev@gmail.com>
2017-08-30 16:58:08 -0300

    Server scafold running...

---
+++ app.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
+const api = require('./routes');
+
+const port = process.env.PORT || 8000;
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+
+app.use(cookieParser());
+
+app.use('/musifypro-api', api);
+
+
+app.listen(port, () => {
+	console.log(`Musify Pro backend API running in port ${port}...`)
+})
